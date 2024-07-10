30ae67f80dab569924eeeb2548a0ac4c92a17041
dhsathiya <devarshisathiya5@gmail.com>
2024-07-10 21:57:31 +0000

    Update doctype name from setting to settings

--- frappe_dev_tools/public/js/navbar_color.js
+++ frappe_dev_tools/public/js/navbar_color.js
@@ -1,4 +1,4 @@
-frappe.db.get_single_value('Developer Setting', 'navbar_background_color').then(color => {
+frappe.db.get_single_value('Developer Settings', 'navbar_background_color').then(color => {
     if(color) {
         // Apply the color to the navbar
         $('.navbar').css('background-color', color);
